968e06dc4525a005d2fb9b741d1cd6caa9d9e6c5
Ritaax7 <rita.tia03@gmail.com>
2024-04-13 18:53:28 +0200

    formattazione test

--- src/Partita.java
+++ src/Partita.java
@@ -1,10 +1,8 @@
 
-
-
 /**
  * Questa classe modella una partita del gioco
  *
- * @author  docente di POO
+ * @author docente di POO
  * @see Stanza
  * @version base
  */
@@ -17,29 +15,29 @@ public class Partita {
 	private Stanza stanzaVincente;
 	private boolean finita;
 	private int cfu;
-	
-	public Partita(){
+
+	public Partita() {
 		creaStanze();
 		this.finita = false;
 		this.cfu = CFU_INIZIALI;
 	}
 
-    /**
-     * Crea tutte le stanze e le porte di collegamento
-     */
-    private void creaStanze() {
+	/**
+	 * Crea tutte le stanze e le porte di collegamento
+	 */
+	private void creaStanze() {
 
 		/* crea gli attrezzi */
-    	Attrezzo lanterna = new Attrezzo("lanterna",3);
-		Attrezzo osso = new Attrezzo("osso",1);
-    	
+		Attrezzo lanterna = new Attrezzo("lanterna", 3);
+		Attrezzo osso = new Attrezzo("osso", 1);
+
 		/* crea stanze del labirinto */
 		Stanza atrio = new Stanza("Atrio");
 		Stanza aulaN11 = new Stanza("Aula N11");
 		Stanza aulaN10 = new Stanza("Aula N10");
 		Stanza laboratorio = new Stanza("Laboratorio Campus");
 		Stanza biblioteca = new Stanza("Biblioteca");
-		
+
 		/* collega le stanze */
 		atrio.impostaStanzaAdiacente("nord", biblioteca);
 		atrio.impostaStanzaAdiacente("est", aulaN11);
@@ -54,14 +52,14 @@ public class Partita {
 		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
 		biblioteca.impostaStanzaAdiacente("sud", atrio);
 
-        /* pone gli attrezzi nelle stanze */
+		/* pone gli attrezzi nelle stanze */
 		aulaN10.addAttrezzo(lanterna);
 		atrio.addAttrezzo(osso);
 
 		// il gioco comincia nell'atrio
-        stanzaCorrente = atrio;  
+		stanzaCorrente = atrio;
 		stanzaVincente = biblioteca;
-    }
+	}
 
 	public Stanza getStanzaVincente() {
 		return stanzaVincente;
@@ -74,17 +72,19 @@ public class Partita {
 	public Stanza getStanzaCorrente() {
 		return this.stanzaCorrente;
 	}
-	
+
 	/**
 	 * Restituisce vero se e solo se la partita e' stata vinta
+	 * 
 	 * @return vero se partita vinta
 	 */
 	public boolean vinta() {
-		return this.getStanzaCorrente()== this.getStanzaVincente();
+		return this.getStanzaCorrente() == this.getStanzaVincente();
 	}
 
 	/**
 	 * Restituisce vero se e solo se la partita e' finita
+	 * 
 	 * @return vero se partita finita
 	 */
 	public boolean isFinita() {
@@ -104,6 +104,6 @@ public class Partita {
 	}
 
 	public void setCfu(int cfu) {
-		this.cfu = cfu;		
-	}	
+		this.cfu = cfu;
+	}
 }
