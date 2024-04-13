8deab5954149476d79b97c75f8f012befb45823a
Sario Tedesco <sariotedesco@gmail.com>
2024-04-13 20:04:53 +0200

    PartitaTest.java

--- test/it/uniroma3/diadia/PartitaTest.java
+++ test/it/uniroma3/diadia/PartitaTest.java
@@ -2,13 +2,62 @@ package it.uniroma3.diadia;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PartitaTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	Partita partita;
+	
+	@BeforeEach
+    void setUp(){
+		partita= new Partita();
+    }
 
+	@Test // Eliminabile
+	void testGetSetCFU() {
+		partita.setCfu(30);
+		assertEquals(partita.getCfu(),30);
+	}
+	
+	@Test
+	void testSetFinita() {
+		partita.setFinita();
+		assertEquals(partita.isFinita(),true);
+	}
+	
+	@Test
+	void testCFUZero() {
+		partita.setCfu(0);
+		assertEquals(partita.isFinita(),true);
+	}
+	
+	@Test
+	void testSetVincente() {
+		partita.setStanzaCorrente(partita.getStanzaVincente());
+		assertEquals(partita.vinta() , true);
+	}
+	
+	@Test
+	void testSetNullCorrente() {
+		partita.setStanzaCorrente(null);
+		assertEquals(partita.vinta() , false);
+	}
+	
+	@Test //controllo vittoria allo start.
+    void testGetStanzaVincente() {
+        assertEquals(false, partita.vinta());
+    }
+	
+	@Test
+	void testSetStanzaNull() {
+		partita.setStanzaCorrente(null);
+		assertNull(partita.getStanzaCorrente());
+	}
+	
+	@Test
+	void testStanzaNullStanzaVincente() {
+		partita.setStanzaCorrente(null);
+		assertNotEquals(partita.getStanzaCorrente() , partita.getStanzaVincente());
+	}
 }
