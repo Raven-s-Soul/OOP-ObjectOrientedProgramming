922c8e7cb95e7d6651a729c827d951ce0aa32198
Sario Tedesco <sariotedesco@gmail.com>
2024-05-19 19:35:57 +0200

    Fix Tests

--- test/it/uniroma3/diadia/DiaDiaSetUpTest.java
+++ test/it/uniroma3/diadia/DiaDiaSetUpTest.java
@@ -1,10 +1,12 @@
 package it.uniroma3.diadia;
 
+import it.uniroma3.diadia.ambienti.LabirintoBuilder;
+
 public class DiaDiaSetUpTest {
 
     public static IOSimulator Simulatore(String[] comandi) {
         IOSimulator io = new IOSimulator(comandi);
-        new DiaDia(io).gioca();
+        new DiaDia( new LabirintoBuilder().getLabirinto(), io).gioca();
         return io;
     }
 
